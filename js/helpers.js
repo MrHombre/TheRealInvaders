af6a159b0be6b9202caff4c6f319745fc1fe57a1
James Lett <jlett89@gmail.com>
2016-08-15 11:00:05 -0700

    Set up js files

---
+++ js/helpers.js
@@ -0,0 +1,29 @@
+// Helpers Functions
+
+
+// Screen
+function Screen(with, height) {
+
+};
+
+Screen.prototype.drawSprite = function (sp, x, y) {
+
+};
+
+
+//Sprite
+function Sprite(img, x, y, w, h) {
+
+};
+
+
+// Input
+function InputHandeler() {};
+
+InputHandeler.prototype.isDown = function (code) {
+
+};
+
+InputHandeler.prototype.isPressed = function (code) {
+
+};
